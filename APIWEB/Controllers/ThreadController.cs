9b681efb9304febbf47e28b17c8dc65b696e1e16
Phùng Đức Anh <183066484+DucAnhSCY@users.noreply.github.com>
2025-03-17 16:12:09 +0700

    Refactor user management in RegisteredUserController
    Completely rewrite the RegisteredUserController with new methods for user management, including retrieval, search, insertion, update, deletion, and login. Introduce DTOs for user operations with validation attributes. Update ThreadController's using directives and add UserController with duplicated functionality from RegisteredUserController.

--- APIWEB/Controllers/ThreadController.cs
+++ APIWEB/Controllers/ThreadController.cs
@@ -1,10 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System.ComponentModel.DataAnnotations;
-using System.Text.Json.Serialization;
-
 using APIWEB.Models;
-
 namespace APIWEB.Controllers
 {
     [ApiController]
