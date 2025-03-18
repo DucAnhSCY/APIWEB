bb85c0a8b4af33bb11df3f407f3d536cbb22a870
Phùng Đức Anh <183066484+DucAnhSCY@users.noreply.github.com>
2025-03-18 16:51:35 +0700

    Remove unused DTOs, HTTP configurations, and related files

--- UpdateUserDTO.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace diendan2.DTOs
-{
-    public class UpdateUserDTO
-    {
-        public string? Username { get; set; }
-        public string? Email { get; set; }
-        public string? Password { get; set; }
-    }
-}
