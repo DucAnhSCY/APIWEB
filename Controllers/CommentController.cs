66bb75ce5bcf41829b654a35e079772c10f00715
Phùng Đức Anh <183066484+DucAnhSCY@users.noreply.github.com>
2025-03-20 14:43:40 +0700

    Fix using directives and clean up CommentUpdateDto
    Restored necessary using directives in `CommentController.cs` to ensure proper access to required namespaces.
    Removed an extraneous closing brace in the `CommentUpdateDto` class to maintain correct syntax and structure.

--- Controllers/CommentController.cs
+++ Controllers/CommentController.cs
@@ -1,13 +1,13 @@
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using diendan2.Models2;
-
 namespace diendan2.Controllers
 {
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.EntityFrameworkCore;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using diendan2.Models2;
+
     [Route("api/[controller]")]
     [ApiController]
     public class CommentController : ControllerBase
